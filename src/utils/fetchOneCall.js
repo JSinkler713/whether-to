13d2403286e89e82ee7bfcf0f7357e70b9ab1887
James Sinkler <jamessinkler713@gmail.com>
2021-04-15 20:58:35 -0700

    cleans up fetchOneCall function

--- src/utils/fetchOneCall.js
+++ src/utils/fetchOneCall.js
@@ -1,8 +1,8 @@
-
-const fetchOneCall = (lat, lng)=> {
-  // all the stuff
-  console.log(lat)
-  console.log(lng)
+const fetchOneCall = async(lat, lng)=> {
+  console.log('fetching weather')
+  let result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+  result = await result.json()
+  return result
 }
 export default fetchOneCall
 
