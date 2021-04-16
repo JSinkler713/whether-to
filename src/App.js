6e71a81318c794a9b85533f42bd846f774513a5c
James Sinkler <jamessinkler713@gmail.com>
2021-04-16 13:39:48 -0700

    save previous user searches to local storage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const [value, setValue] = useState('')
   const [coords, setCoords] = useState({ lat:null, lng:null })
   const [weather, setWeather] = useState({})
+  const [previousSearches, setPreviousSearches] = useState([])
+
+  useEffect(()=> {
+    // on component load get the previous searches out of local storage
+    setPreviousSearches( JSON.parse(localStorage.getItem('searches')) )
+  }, [])
 
   useEffect(()=> {
     // when lat and lng update call our OneCallApi
@@ -18,9 +24,26 @@ function App() {
     getWeather()
   }, [coords])
 
+  useEffect(()=> {
+    // when a user enters a search save it to lacalstorage
+    let searches = localStorage.getItem('searches')
+    searches = JSON.parse(searches)
+    if (searches && searches.length > 0) {
+      searches = searches.filter((item)=> item!== value)
+    } else {
+      searches = []
+    }
+    const updatedSearches = [
+      ...searches,
+      value
+    ]
+    localStorage.setItem('searches', JSON.stringify(updatedSearches))
+    setPreviousSearches(updatedSearches)
+  }, [coords])
+
   return (
     <div className="App">
-      <PlacesAutocomplete setParentValue={setValue} setParentCoords={setCoords}/>
+      <PlacesAutocomplete setParentValue={setValue} setParentCoords={setCoords} previousSearches={previousSearches}/>
     </div>
   );
 }
