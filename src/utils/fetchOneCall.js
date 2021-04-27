f6ca13293d5e6de717a4caa1c8372ca56dab135c
James Sinkler <jamessinkler713@gmail.com>
2021-04-27 13:23:26 -0700

    makes default fetch hardcoded for Imperial Units, F/mph

--- src/utils/fetchOneCall.js
+++ src/utils/fetchOneCall.js
@@ -1,6 +1,6 @@
 const fetchOneCall = async(lat, lng)=> {
   console.log('fetching weather')
-  let result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+  let result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=imperial&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
   result = await result.json()
   return result
 }
