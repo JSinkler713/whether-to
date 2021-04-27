fc3d5e9768461514eb92178cb4912202575a3925
James Sinkler <jamessinkler713@gmail.com>
2021-04-27 14:55:57 -0700

    show stats in human readable form

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useContext} from 'react'; 
 import styled from 'styled-components'
 import calculateBackground from './utils/calculateBackground'
+import calculateTime from './utils/calculateTime'
 
 const CurrentWeatherWrapper = styled.div`
  height: 100%;
@@ -8,7 +9,7 @@ const CurrentWeatherWrapper = styled.div`
  icon: icon;
 `
  
-const CurrentWeather = ({weather})=> { 
+const CurrentWeather = ({weather, place})=> { 
     let icon
     if (weather.current) {
         icon = weather.current.weather[0].icon
@@ -18,8 +19,14 @@ const CurrentWeather = ({weather})=> {
         <CurrentWeatherWrapper icon={weather.current.weather[0].icon}>
         { weather.current ? (
         <div>
+            <p>{place.split(',')[0]}</p>
             <p>{weather.current.weather[0].main}</p>
             <p>{Math.floor(weather.current.temp)} F</p>
+            <p>{calculateTime(weather.current.dt, weather.timezone_offset)}</p>
+            <p>Precipitation {weather.minutely ? weather.minutely[0].precipitation * 100 : weather.hourly[0].pop * 100}%</p>
+            <p>Humidity {weather.current.humidity}%</p>
+            <p>UV Index {Math.floor(weather.current.uvi)} of 10</p>
+            <p>Wind {Math.round(weather.current.wind_speed)} mph</p>
         </div>)
         :
         <>
