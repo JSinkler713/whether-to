70b7f19487540b37397275c235ec124e57b40204
James Sinkler <jamessinkler713@gmail.com>
2021-04-29 08:33:00 -0700

    uses calculateIcon, to display correct icon for current weather

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,13 +1,30 @@
 import React, {useState, useEffect, useContext} from 'react'; 
 import styled from 'styled-components'
 import calculateBackground from './utils/calculateBackground'
+import calculateIcon from './utils/calculateIcon'
 import calculateTime from './utils/calculateTime'
+// import clear from './assets/01d.svg'
+// import cloud from './assets/03.svg'
+// import rain from './assets/09.svg'
+// import thunder from './assets/11.svg'
 
 const CurrentWeatherWrapper = styled.div`
  height: 100%;
  width: 100%;
  icon: icon;
 `
+const IconWrapper = styled.div`
+width: 100%;
+margin: 0 auto;
+`
+/*
+const Icon = styled.img`
+width: 100px;
+height: 100px;
+src: cloud;
+src: TODO not resolving;
+`
+*/
  
 const CurrentWeather = ({weather, place})=> { 
     let icon
@@ -20,6 +37,9 @@ const CurrentWeather = ({weather, place})=> {
         { weather.current ? (
         <div>
             <p>{place.split(',')[0]}</p>
+          <IconWrapper>
+            <img src={calculateIcon(icon)} />
+          </IconWrapper>
             <p>{weather.current.weather[0].main}</p>
             <p>{Math.floor(weather.current.temp)} F</p>
             <p>{calculateTime(weather.current.dt, weather.timezone_offset)}</p>
