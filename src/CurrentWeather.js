42b6f0781f2df087b7d39844e3b4d86cc3f98114
James Sinkler <jamessinkler713@gmail.com>
2021-04-30 11:13:41 -0700

    style bottom stats

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -18,6 +18,47 @@ const IconWrapper = styled.div`
 width: 100%;
 margin: 0 auto;
 `
+const InfoBlock = styled.div`
+display: flex;
+justify-content: flex-end;
+`
+const InfoWrapper = styled.div`
+margin-right: -10px;
+color: black;
+width: 230px;
+border-radius: 4px 0px 0px 128px;
+background: #FFF; 
+display: grid;
+grid-template-columns: auto 80px;
+`
+const StatNames = styled.div`
+padding: 10px 0px;
+font-family: Source Sans Pro, sans-serif;
+font-style: normal;
+font-weight: normal;
+font-size: 18px;
+line-height: 21px;
+/* identical to box height */
+
+text-align: right;
+
+color: #6A6A6A;
+`
+const StatProperties = styled.div`
+padding: 10px 0px;
+font-family: Source Sans Pro, sans-serif;
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 21px;
+/* identical to box height */
+margin-left: 1rem;
+
+text-align: left;
+
+color: #000;
+
+`
 /*
 const Icon = styled.img`
 width: 100px;
@@ -41,13 +82,29 @@ const CurrentWeather = ({weather, place})=> {
           <IconWrapper>
             <img src={calculateIcon(icon)} />
           </IconWrapper>
+          {/*
             <p>{weather.current.weather[0].main}</p>
             <p>{Math.floor(weather.current.temp)} F</p>
             <p>{calculateTime(weather.current.dt, weather.timezone_offset)}</p>
-            <p>Precipitation {weather.minutely ? weather.minutely[0].precipitation * 100 : weather.hourly[0].pop * 100}%</p>
-            <p>Humidity {weather.current.humidity}%</p>
-            <p>UV Index {Math.floor(weather.current.uvi)} of 10</p>
-            <p>Wind {Math.round(weather.current.wind_speed)} mph</p>
+            */}
+          <InfoBlock>
+          <InfoWrapper>
+            <StatNames>
+            <p>Precipitation</p>
+            <p>Humidity</p>
+            <p>UV Index</p>
+            <p>Wind</p>
+            </StatNames>
+            <StatProperties>
+            <p>{weather.minutely ? weather.minutely[0].precipitation * 100 : weather.hourly[0].pop * 100}%</p>
+            <p>{weather.current.humidity}%</p>
+            <p>{Math.floor(weather.current.uvi)} of 10</p>
+            <p>{Math.round(weather.current.wind_speed)} mph</p>
+
+
+            </StatProperties>
+          </InfoWrapper>
+          </InfoBlock>
         </div>)
         :
         <>
