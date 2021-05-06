8619e2c220d5c141a4484bf5e53893905b957656
James Sinkler <jamessinkler713@gmail.com>
2021-05-06 14:19:08 -0700

    FIX: access icons for forecast

--- src/ForecastDays.js
+++ src/ForecastDays.js
@@ -14,7 +14,7 @@ padding: 15px 10px 15px 5px;
  
 const ForecastDays = (props)=> { 
   const listOfForecastDays = props.days.map((day, i)=> (
-    <SingleDayForecast icon={day.weather.icon} sunrise={day.sunrise} offset={props.offset} high={day.temp.max} low={day.temp.min} allInfo={day}/>
+    <SingleDayForecast icon={day.weather[0].icon} sunrise={day.sunrise} offset={props.offset} high={day.temp.max} low={day.temp.min} allInfo={day}/>
   ))
   return ( 
      <ForeCastWrapper> 
